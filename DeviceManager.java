97097401af34256bcfa8eb7ed49a14e3c6ec6f00
FelixMachado <felix.machado@campus.mccd.edu>
2025-04-30 19:00:26 +0000

    RecomCorp 1.1

--- DeviceManager.java
+++ DeviceManager.java
@@ -4,16 +4,24 @@
 * FinalProject
 **/
 
-import java.io.BufferdReader;
+
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**This class will be used to import data from the CSV files
+ * and create the objects that will be referenced by the main class
+ * using an Arraylist.
+ * */
 
 
 public class DeviceManager {
 
     public static void main (String[] args){
 
-
+       //I used ChatGpt to help me find a way to load multiple csv files
 
 
 
